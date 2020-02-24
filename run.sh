550974a98169f6b5ffbb4af261e2f08adb551ef1
Vassiliy Yegorov <vasyakrg@gmail.com>
2020-02-24 11:18:58 +0700

    init commit

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+[ ! -f .env ] && {
+    SECRET=$(tr -dc 'a-f0-9' < /dev/urandom | dd bs=1 count=32 2>/dev/null)
+    echo -"SECRET=$SECRET" > .env
+}
+
+docker-compose pull mtproxy
+docker stop mtproxy
+docker rm mtproxy
+docker-compose up -d
