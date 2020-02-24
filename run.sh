80ca97cafb9574dcfff85c42a65f5719b409ac99
Vassiliy Yegorov <vasyakrg@gmail.com>
2020-02-24 11:36:10 +0700

    add promo tag

--- run.sh
+++ run.sh
@@ -2,12 +2,14 @@
 
 FOLDER=/srv/services/proxy-config
 PORT=1945
+TAG=null
 
 [ ! -f .env ] && {
     SECRET=$(tr -dc 'a-f0-9' < /dev/urandom | dd bs=1 count=32 2>/dev/null)
     echo "SECRET=$SECRET" > .env
     echo "FOLDER=${FOLDER}" >> .env
     echo "PORT=${PORT}" >> .env
+    echo "TAG=${TAG}" >> .env
 }
 
 
