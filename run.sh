49262f2e0fc4ffc100d4e96e7430bb81734e1b69
Vassiliy Yegorov <vasyakrg@gmail.com>
2020-02-24 11:21:06 +0700

    fix echo

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 [ ! -f .env ] && {
     SECRET=$(tr -dc 'a-f0-9' < /dev/urandom | dd bs=1 count=32 2>/dev/null)
-    echo -"SECRET=$SECRET" > .env
+    echo "SECRET=$SECRET" > .env
 }
 
 docker-compose pull mtproxy
