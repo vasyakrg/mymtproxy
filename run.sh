58532e1963d4bb7f9161c41dce1fb82228416e12
Vassiliy Yegorov <vasyakrg@gmail.com>
2020-02-24 11:30:30 +0700

    update envs

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+FOLDER=/srv/services/proxy-config
+PORT=1945
+
 [ ! -f .env ] && {
     SECRET=$(tr -dc 'a-f0-9' < /dev/urandom | dd bs=1 count=32 2>/dev/null)
     echo "SECRET=$SECRET" > .env
+    echo "FOLDER=${FOLDER}" >> .env
+    echo "PORT=${PORT}" >> .env
 }
+
 
 docker-compose pull mtproxy
 docker stop mtproxy
